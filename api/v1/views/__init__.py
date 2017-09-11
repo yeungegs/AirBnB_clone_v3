c72bdc0ec3644deba857b534f4993a19d52aacd5
Carrie Ybay <carrieybay@gmail.com>
2017-09-11 13:34:24 -0700

    Add import for state views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ Views for AirBnB_clone_v3
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
+from api.v1.views.states import *
