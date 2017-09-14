9ad1c5503a86c4f4f4521c60c0a87ec917845f35
Carrie Ybay <carrieybay@gmail.com>
2017-09-13 20:45:54 -0700

    Add tests for documentation and fix PEP8 formatting

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -70,6 +70,18 @@ class TestFileStorageDocs(unittest.TestCase):
         actual = FileStorage.reload.__doc__
         self.assertEqual(expected, actual)
 
+    def test_doc_get(self):
+        """... documentation for get function"""
+        expected = ' retrieves one object '
+        actual = DBStorage.get.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_count(self):
+        """... documentation for count function"""
+        expected = ' counts number of objects in storage '
+        actual = DBStorage.count.__doc__
+        self.assertEqual(expected, actual)
+
 
 @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
 class TestBmFsInstances(unittest.TestCase):
